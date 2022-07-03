0478451379788aa5f8dfac9bbdb612bac1a1c206
ivansanchez90 <sanchezoliva.ivan@gmail.com>
2022-07-03 16:02:15 -0300

    consultando a la api de cryptos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,3 +1,4 @@
+import {useEffect} from 'react'
 import styled from "@emotion/styled"
 import useSelectMonedas from "../hooks/useSelectMonedas"
 import {monedas} from '../data/monedas'
@@ -13,6 +14,7 @@ const InputSubmit = styled.input`
     font-size: 20px;
     border-radius: 5px;
     transition: background-color .3s ease;
+    margin-top: 30px;
     &:hover {
         background-color: #7a7dfe;
         cursor: pointer;
@@ -21,11 +23,25 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
 
-    const [SelectMonedas] = useSelectMonedas('Elige tu Moneda', monedas)
+    const [moneda, SelectMonedas] = useSelectMonedas('Elige tu Moneda', monedas)
+
+    useEffect(() => {
+      const consultarApi = async () => {
+        const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+        console.log(resultado.Data);
+        
+
+      }
+      consultarApi();
+    }, [])
+    
     
   return (
     <form>
         <SelectMonedas />
+        
         <InputSubmit type="submit" value="Cotizar" />
     </form>
   )
